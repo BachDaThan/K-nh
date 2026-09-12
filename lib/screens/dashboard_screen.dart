5f15c68f5bda72c52cde5c422ad592467f6e2b18
BachDaThan <BachDaThan@users.noreply.github.com>
2026-09-12 16:03:38 +0700

    Fix: thiếu import shared_preferences trong dashboard_screen.dart

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../models/bento_item.dart';
 import '../widgets/bento_card.dart';
 import '../browser/screens/browser_screen.dart';
